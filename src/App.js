6e6359da74001c6d85940e87b81918d4ae6c901c
ElizAbeth <lenguaje.pecz@gmail.com>
2024-10-19 16:46:57 -0500

    Se coloca el tabs en el app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import './App.css';
 import Formulario from './components/Formulario/Formulario';
 import Empresa from './components/Empresa/Empresa';
 import TabsCard from './components/Tabs/TabsCard';
+import './TabsCard.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {TabContent, TabPane, Nav, NavItem, NavLink} from "reactstrap";
+
+
+
 
 function App() {
   const [mostrarFormulario, actualizarMostrar] = useState(false);
@@ -39,20 +45,113 @@ function App() {
     actualizarEquipos(equiposActualizados);
   };
 
+  const[activeTab,setActiveTab] = useState("1");
+
+  const cambiarTab = (numeroTab) =>{
+    if(activeTab !== numeroTab){
+      setActiveTab(numeroTab);
+    }
+  }
+
 
   return (
     <div>
-      <TabsCard />
-      {equipos.map((equipo) => (
-        <Empresa
-          datos={equipo}
-          key={equipo.titulo}
-          colaboradores={colaboradores.filter(colaborador => colaborador.equipo === equipo.titulo)}
-          actualizarColor={actualizarColor}
+          <div className="App">
+       <Nav tabs>
+         <NavItem>
+           <NavLink 
+           className={(activeTab=="1" ? "activeTab baseTab" : "baseTab" )}
+           onClick={()=>cambiarTab("1")}>
+             MI PERFIL
+           </NavLink>
+         </NavItem>
 
-        />
+         <NavItem>
+           <NavLink 
+              className={(activeTab=="2" ? "activeTab baseTab" : "baseTab" )}
+           onClick={()=>cambiarTab("2")}>
+             MI EMPRESA
+           </NavLink>
+         </NavItem>
+
+         <NavItem>
+           <NavLink 
+              className={(activeTab=="3" ? "activeTab baseTab" : "baseTab" )}
+           onClick={()=>cambiarTab("3")}>
+            PRODUCTOS
+           </NavLink>
+         </NavItem>
+
+         <NavItem>
+           <NavLink 
+              className={(activeTab=="4" ? "activeTab baseTab" : "baseTab" )}
+           onClick={()=>cambiarTab("4")}>
+            EVENTOS
+           </NavLink>
+         </NavItem>
+
+         <NavItem>
+           <NavLink 
+              className={(activeTab=="5" ? "activeTab baseTab" : "baseTab" )}
+           onClick={()=>cambiarTab("5")}>
+            CONTACTOS
+           </NavLink>
+         </NavItem>
+
+       </Nav>
+
+       <TabContent activeTab={activeTab}>
+         <TabPane tabId="1">
+           <div className='container'>
+             <br />
+             {equipos.map((equipo) => (
+              <Empresa
+                datos={equipo}
+                key={equipo.titulo}
+                colaboradores={colaboradores.filter(colaborador => colaborador.equipo === equipo.titulo)}
+                actualizarColor={actualizarColor}
+
+              />
         
       ))}
+           </div>
+
+         </TabPane>
+
+         <TabPane tabId="2">
+           {/*<div className='container'>
+             <br />
+             <img src='https://lh3.googleusercontent.com/a-/AOh14GjjMfH-BpCvLrIU7AoboHTinGSIgLowZtLIGrJTFg=s360-p-rw-no'
+              width={300} height={300}
+              />
+           </div>*/}
+
+         </TabPane>
+         
+
+
+         <TabPane tabId="3">
+           
+         {/*<Productos/>*/}
+         </TabPane>
+
+         <TabPane tabId="4">
+           {/*<div className='container'>
+             <br />
+             <p>Este es un evento</p>
+           </div>*/}
+         </TabPane>
+
+         <TabPane tabId="5">
+           {/*<div className='container'>
+             <br />
+             <Contacto/>
+           </div>*/}
+         </TabPane>
+
+       </TabContent>
+    </div>
+      
     </div>
     
   );
